6c99785a2b234cf4241dcc7c51733c6167999a4b
Wellington  Rocha <79056091+Wellington1980@users.noreply.github.com>
2021-05-16 16:27:25 -0300

    Add files via upload

---
+++ Cadastro de Anuncios.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<stdio.h>
+#difine SIZE 200
+
+char nome do anuncio[SIZE][50];
+char cliente [SIZE][50];
+float data de inicio[SIZE][50];
+float data de termino[SIZE][50];
+float investimento por dia[SIZE][50];
+int op
+
+}
+void cadastro(){
+	
+	do{
+		print("\n Digite o nome do Cliente:");
+		scanf("%s", &nome do Cliente);
+
+		print("\n  Cliente:");
+		scanf("%s", &cliente);
+
+		print("\n data de inicio:");
+		scanf("%d", &data de inicio);
+
+		print("\n data de termino:");
+		scanf("%d", &data de termino);
+		
+		printf("\n Digite 1 para continuar ou outro valor para sair!!")
+		
+		scanf ("%d", &op);
+		
+		
+		while(op==1);
+	}
+}
+
+
+
+
+// programa para cadastro de anunciantes de algum site
+
+int main(void) {
+	
+}
---
+++ Calc Online.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib
+
+// Codigo para calcular em Média de pessoas que acessam ao site por valor investido 
+
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+	float visualisaram, curtiram , compartilharam, não compartilharam,valor, media;
+	
+	printf ("Quantos visualisaram? ");
+	scanf ("%f", &visualisaram - 12% );
+	// Calculo do numero de vizualizações menos 12%, media de quantos curtem 
+	
+	printf ("Quantos curtiram? ");
+	scanf ("%f", &curtiram -  );
+	
+	printf ("Quantos compartilharam? ");
+	scanf ("%f", &curtiram - compartilharam = media );
+	
+	printf ("Quantos não compartilharam? ");
+	scanf ("%f", &não compartilharam - 30% );
+	
+	soma = (visualisaram, curtiram , compartilharam, não compartilharam);
+	
+	printf (" O total investido por pessoa foi  R$" soma   );
+	
+	
+	
+}
